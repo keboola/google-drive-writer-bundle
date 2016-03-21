29b8b9bf81ce2af3076aa085b28d740f10f731b1
Miro Cillik <miro@keboola.com>
2016-03-21 16:56:11 +0000

    fix: small fixes, increased backoff on Rest Client

--- GoogleDrive/RestApi.php
+++ GoogleDrive/RestApi.php
@@ -36,7 +36,7 @@ class RestApi
 	public function __construct(GoogleApi $api, EngineInterface $templating)
 	{
 		$this->api = $api;
-		$this->api->setBackoffsCount(9);
+		$this->api->setBackoffsCount(10);
 		$this->templating = $templating;
 	}
 
